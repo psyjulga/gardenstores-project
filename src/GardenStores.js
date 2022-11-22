9b8ed93adc4c004c6f3075cc02e47737e6a5d0ad
👩Ladybug🐞 <psyjulga@yahoo.de>
2022-11-22 15:52:10 +0100

    refactored to data.json

--- src/GardenStores.js
+++ src/GardenStores.js
@@ -1,75 +1,20 @@
+// import { useState, useEffect } from 'react'
 import GardenStore from './GardenStore'
 
 const GardenStores = ({ mobileView }) => {
-	const url = 'https://github.com/psyjulga/gardenstores-project'
+	// const [gardenStores, setGardenStores] = useState([])
 
-	const gardenStores = [
-		{
-			rating: 3,
-			name: "Fool's Garden",
-			trees: 'y',
-			plants: 'y',
-			bio: 'n',
-			address: {
-				street: 'Main Street 2',
-				zip: 12345,
-				city: 'Somewhere',
-				website: url,
-			},
-		},
-		{
-			rating: 4.5,
-			name: 'Savage Garden',
-			trees: 'n',
-			plants: 'y',
-			bio: 'n',
-			address: {
-				street: 'Flower Street 2',
-				zip: 55545,
-				city: 'Nowhere',
-				website: url,
-			},
-		},
-		{
-			rating: 4,
-			name: 'Magic Garden',
-			trees: 'y',
-			plants: 'y',
-			bio: 'y',
-			address: {
-				street: 'Diagon Alley 2',
-				zip: 12321,
-				city: 'Hogwarts',
-				website: url,
-			},
-		},
-		{
-			rating: 2.5,
-			name: "King's Garden",
-			trees: 'n',
-			plants: 'y',
-			bio: 'y',
-			address: {
-				street: 'Queen Street 2',
-				zip: 55555,
-				city: "Salem's Lot",
-				website: url,
-			},
-		},
-		{
-			rating: 5,
-			name: 'Garden Heaven',
-			trees: 'y',
-			plants: 'y',
-			bio: 'y',
-			address: {
-				street: 'Heaven Street 2',
-				zip: 12121,
-				city: 'Everywhere',
-				website: url,
-			},
-		},
-	]
+	const gardenStoresJson = require('./fake_api/data.json')
+	const gardenStores = gardenStoresJson['GardenStores']
+
+	// useEffect(() => {
+	// 	fetch(path)
+	// 		.then((res) => {
+	// 			console.log('RES: ', res)
+	// 			res.json()
+	// 		})
+	// 		.then((gardenStores) => setGardenStores(gardenStores))
+	// }, [])
 
 	return (
 		<ul className="garden-stores">
