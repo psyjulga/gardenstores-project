d95291855d3318f791763f5950eff54d0d482fec
👩Ladybug🐞 <psyjulga@yahoo.de>
2022-11-22 21:30:10 +0100

    refactored readme

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css'
 import Navbar from './Navbar'
 import ImageSlide from './ImageSlide'
 import GardenStores from './GardenStores'
+import Footer from './Footer'
 
 function App() {
 	const mobileView = window.innerWidth <= 645
@@ -11,6 +12,7 @@ function App() {
 			<Navbar mobileView={mobileView} />
 			<ImageSlide />
 			<GardenStores mobileView={mobileView} />
+			<Footer />
 		</div>
 	)
 }
---
+++ src/Footer.js
@@ -0,0 +1,17 @@
+const Footer = () => {
+	return (
+		<footer className="footer">
+			<a
+				href="https://github.com/psyjulga/gardenstores-project"
+				target="_blank"
+				rel="noreferrer"
+				title="Code on Github"
+			>
+				Open-source code
+			</a>
+			🐞by Julia Bestgen💃
+		</footer>
+	)
+}
+
+export default Footer
